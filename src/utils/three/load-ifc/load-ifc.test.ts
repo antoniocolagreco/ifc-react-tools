252b2f3df335885ad76e67dcb26d1f47b20e2db1
Antonio Colagreco <92807691+antoniocolagreco@users.noreply.github.com>
2024-12-05 15:48:05 +0100

    fix the ifc loader basic test

--- src/utils/three/load-ifc/load-ifc.test.ts
+++ src/utils/three/load-ifc/load-ifc.test.ts
@@ -1,3 +1,4 @@
+import { IfcProgressEvent } from '@/classes/ifc-progress-event'
 import path from 'node:path'
 import { describe, expect, it } from 'vitest'
 import { loadIfc } from './load-ifc'
@@ -5,17 +6,20 @@ import { loadIfc } from './load-ifc'
 describe('load-ifc', () => {
 	it('should load the 3d model', async () => {
 		const url = path.resolve('public/test/', 'castle.ifc')
+		let lastEvent: IfcProgressEvent = new IfcProgressEvent()
+
 		await loadIfc(
 			url,
 			() => {},
 			event => {
 				console.log(event.step, event.type, event.loaded, event.total)
+				lastEvent = event
 			},
 			error => {
 				console.error(error)
 			},
 		)
 
-		expect(1).toBe(1)
+		expect(lastEvent.type).toBe('done')
 	})
 })
